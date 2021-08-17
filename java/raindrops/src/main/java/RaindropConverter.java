0ce963657f095288a09e6cb9ab0e0bc7790da502
senred <senouci.reda@gmail.com>
2021-08-17 20:42:40 +0200

    Refacto of code

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,19 @@
+import java.util.Map;
+import java.util.stream.Collectors;
+
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(number == 0)
+            return "0";
+        if(number == 1)
+            return "1";
+        Map<Integer,String> numberMapping =
+                Map.of(3,"Pling",5,"Plang",7,"Plong");
+        return numberMapping.entrySet().stream()
+        .filter(entry -> entry.getKey() % number == 0)
+        .map(entry -> numberMapping.get(entry))
+        .collect(Collectors.joining());
     }
 
 }
